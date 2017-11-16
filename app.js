e6c2b3704a2f2042c86024d7e265b5e870e7e2f2
Maria Virginia Vitali Salas <33062827+Marivit@users.noreply.github.com>
2017-11-15 20:58:45 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+
+const path = require('path');
+
+//Inicializo el servidor express
+var express=require('express');
+var app= express();
+
+
+//Set static Path
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+//Configuración de la aplicación express
+require('./app/config/set-config-express')(app);
+
+
+
+//Exporto mi instancia de app para utilizarlo en otros archivos
+module.exports = app;
+ 
+//Rutas de mi aplicación
+//require(path.join(process.cwd(), 'app', 'routes'))();
+
+
+app.get("/",function (req, res) {
+
+    //try {
+      return res.render('crearHistoriaCardio');
+   // } catch (e) {
+    //  console.log("Error");
+    });
+//Ejecuto el servidor
+app.listen(8080);
+
+
